ef6308db5b485c0bbde2b09e1f5e57c23c357709
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-10 14:08:51 -0500

    Improve UX by color coding income and expense inputs. Finish application

--- app.js
+++ app.js
@@ -151,6 +151,12 @@ var UIController = (function(){
             return sign + " " + int + "." + dec;
         };
     
+     var nodeListForEach = function(list, callback){
+            for(let i = 0; i < list.length; i++){
+                callback(list[i], i);
+            }
+        };
+    
     return {
         getInput: function(){
             return {
@@ -207,11 +213,6 @@ var UIController = (function(){
         displayPercentages: function(percentages){
            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
             
-            var nodeListForEach = function(list, callback){
-                for(let i = 0; i < list.length; i++){
-                    callback(list[i], i);
-                }
-            }
             nodeListForEach(fields, function(current, index){
                 if(percentages[index] > 0)
                     current.textContent = percentages[index] + "%";
@@ -226,6 +227,17 @@ var UIController = (function(){
             month = allMonths[now.getMonth()];
             year = now.getFullYear();
             document.querySelector(DOMstrings.dateLabel).textContent = month + " " +year;
+            
+        },
+        changedType: function(){
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + "," +
+                DOMstrings.inputDescription + ","+
+                DOMstrings.inputValue);
+            nodeListForEach(fields, function(curr){
+                curr.classList.toggle("red-focus");
+            });
+             document.querySelector(DOMstrings.inputBtn).classList.toggle("red");
             
         }
     }
@@ -246,8 +258,9 @@ var controller = (function(budgetCtrl, UICtrl){
     });
     document.querySelector(DOM.container).addEventListener("click", ctrlDeleteItem);
         
+    document.querySelector(DOM.inputType).addEventListener("change", UICtrl.changedType);
         
-    }
+    };
    
     
     var ctrlAddItem = function(){
