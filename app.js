8b25780b4b0515ea4af65c9d1c657a3fc2f65add
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-10 13:13:22 -0500

    Update UI with new expense percentages. Use custom forEach function for nodes

--- app.js
+++ app.js
@@ -125,7 +125,8 @@ var UIController = (function(){
         incomeLabel: ".budget__income--value",
         expensesLabel: ".budget__expenses--value",
         percentageLabel: ".budget__expenses--percentage",
-        container: ".container"
+        container: ".container",
+        expensesPercLabel: ".item__percentage"
         
     };
     return {
@@ -178,6 +179,21 @@ var UIController = (function(){
            if(obj.percentage > 0) document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage +"%";
             else
              document.querySelector(DOMstrings.percentageLabel).textContent = "---"   
+        },
+        displayPercentages: function(percentages){
+           var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+            
+            var nodeListForEach = function(list, callback){
+                for(let i = 0; i < list.length; i++){
+                    callback(list[i], i);
+                }
+            }
+            nodeListForEach(fields, function(current, index){
+                if(percentages[index] > 0)
+                    current.textContent = percentages[index] + "%";
+                else
+                    current.textContent = "---";
+            });
         }
     }
 })();
@@ -252,8 +268,8 @@ var controller = (function(budgetCtrl, UICtrl){
         budgetCtrl.calculatePercentages();
         // read % from budgetController
         var percentages = budgetCtrl.getPercentages();
-        console.log(percentages);
-        // update UI
+        //update UI
+        UICtrl.displayPercentages(percentages);
     }
     
    
