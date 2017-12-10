9cd4b45a15052955353f4804e2030f36e30d3849
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-10 09:07:40 -0500

    Improve income and expense label display

--- app.js
+++ app.js
@@ -86,7 +86,12 @@ var UIController = (function(){
         inputValue: ".add__value",
         inputBtn: ".add__btn",
         incomeContainer: ".income__list",
-        expensesContainer: ".expenses__list"
+        expensesContainer: ".expenses__list",
+        budgetLabel: ".budget__value",
+        incomeLabel: ".budget__income--value",
+        expensesLabel: ".budget__expenses--value",
+        percentageLabel: ".budget__expenses--percentage"
+        
     };
     return {
         getInput: function(){
@@ -126,6 +131,15 @@ var UIController = (function(){
                 current.value = "";
             });
             fieldsArr[0].focus();
+        },
+        displayBudget: function(obj){
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            
+           if(obj.percentage > 0) document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage +"%";
+            else
+             document.querySelector(DOMstrings.percentageLabel).textContent = "---"   
         }
     }
 })();
@@ -169,11 +183,18 @@ var controller = (function(budgetCtrl, UICtrl){
         // return budget
         var budget = budgetCtrl.getBudget();
         // display the budget on the UI
+        UICtrl.displayBudget(budget);
     }
    
     return {
         init: function(){
             console.log("application has started");
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setUpEventListeners();
         }
     }
